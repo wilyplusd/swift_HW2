1a2c1b007868aefc4d2951d83aba087b6718d4d1
Dervyanko <wilyplus@gmail.com>
2023-03-19 19:02:04 +0300

    fix bugs

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -8,13 +8,11 @@
 import UIKit
 
 class ProfileView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+   
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    @IBOutlet weak var avatarImage: UIImage!
+    
 }
